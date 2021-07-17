e95a306219603c821928349e642009fb31e9d9ef
Denperidge <denperidge@gmail.com>
2021-07-17 12:35:55 +0200

    Config now saved & loaded to/from url

--- src/input.js
+++ src/input.js
@@ -17,27 +17,45 @@ $('.value')
 
 // Visualize changes entered by the user
 $('.config')
-// TODO https://stackoverflow.com/a/56777426
-    .focusout((e) => {
-        // Closest can fire on parents as well as self, neat
-        var configElement = $(e.target);
-        configElement.hide();
+    .focusout(UpdateConfig);
 
-        var valueElement = configElement.prev('.value');
-        var configValue = configElement.val();
+const params = new URLSearchParams(location.search);
+function UpdateConfig(e) {
+    // Closest can fire on parents as well as self, neat
+    var configElement = $(e.target);
+    configElement.hide();
 
-        if (e.target.className.includes('tag')) {
-            configElement.parent('div').hide();
-            configValue = configElement.closest('tags').value;
-        }
+    var valueElement = configElement.prev('.value');
+    var configValue = configElement.val();
 
-        console.log(configValue);
-        if (configValue == "") configValue = "...";
+    if (e.target.className.includes('tag')) {
+        configElement.parent('div').hide();
+        configValue = configElement.closest('tags').value;
+    }
+
+    console.log(configValue);
+    if (configValue == "") configValue = "...";
 
 
-        valueElement.text(configValue).show();
+    valueElement.text(configValue).show();
 
-        // Set universal data
-        data[valueElement.attr('id')] = configValue;
-        console.log(data)
-    });
+    // Set universal data
+    data[valueElement.attr('id')] = configValue;
+    console.log(data)
+
+    // Set values in URL
+    // https://stackoverflow.com/a/56777426
+    params.set(valueElement.attr('id'), configValue);
+    window.history.replaceState({}, '', location.pathname + "?" + params.toString());
+}
+
+// On startup, set values
+var paramEntries = params.entries();
+var entry;
+while (!(entry = paramEntries.next()).done) {
+    console.log(entry)
+    var configElement = $("#" + entry.value[0]).next(".config");
+    console.log(configElement)
+    configElement.val(entry.value[1]);
+    configElement.focusout();
+}
